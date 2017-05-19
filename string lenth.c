ddce14d29e3260596f08bb71cd4801288a919222
Rsulakhe <rushikeshsulakhe24@gmail.com>
2017-05-19 21:55:04 +0530

    string lenth

---
+++ string lenth.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int len;
+char str1[25]="Hi,How are you baby";
+char str2[40]="you have very beautiful eyes";
+len= strlen(str1);
+printf("strlen(str1) : %d\n",len);
+len = strlen(str2);
+printf("strlen(str1) : %d\n",len);
+}
